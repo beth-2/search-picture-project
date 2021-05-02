a49a207a737ee05f41a0a8f3492d4219a58208bb
beth-2 <77358378+beth-2@users.noreply.github.com>
2021-05-02 20:04:40 +0300

    Material-ui Added

--- src/component/App.js
+++ src/component/App.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Container, Typography, CssBaseline } from "@material-ui/core";
+import PanoramaIcon from '@material-ui/icons/Panorama';
 
 const App = () => {
-    return (
-        <div>
-            React Pic Search
-        </div>
-    )
-}
-export default App
+  return (
+    <>
+      <CssBaseline />
+      <Container maxWidth="sm">
+      <Typography variant="h4" gutterBottom>
+      <PanoramaIcon style={{fontSize: 40, padding: 5}}/>
+        React Pic Search
+      </Typography>
+      </Container>
+    </>
+  );
+};
+export default App;
